6edd4f62a2be31a9b815e14103ae5cf5766650e0
Thomas Lewis <thomasbartlewis@gmail.com>
2019-11-01 23:59:15 -0600

    test helper reads files from test directory

--- src/test/testHelper.ts
+++ src/test/testHelper.ts
@@ -1,10 +1,20 @@
 import { TextDocument } from 'vscode-languageserver';
-import { readdirSync } from 'fs';
+import { readdirSync, readFile, readFileSync } from 'fs';
+import { isObject } from 'util';
+import { resolve } from 'path';
+import fileUrl = require('file-url');
 
 let testFiles = new Map<string, TextDocument>()
-
+console.log(__dirname)
 try {
-	let testFileNames = readdirSync('./krlTestFiles')
+	let testFileNames = readdirSync(resolve(__dirname, './krlTestFiles'))
+	testFileNames.forEach(testFileName => {
+		let testFilePath = resolve(__dirname, 'krlTestFiles', testFileName)
+		const fileContent = readFileSync(testFilePath, 'utf8')
+		let fileURI = fileUrl(testFilePath)
+		let document = TextDocument.create(fileURI, 'krl', 0, fileContent)
+		testFiles.set(testFileName, document)
+	});
 } catch (e) {
 	console.error('Unable to load test files')
 	console.error(e)
